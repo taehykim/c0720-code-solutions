71e37377bb4cb1397fe17c0f827534006d49f311
Tia <tiarekim@gmail.com>
2020-09-04 00:06:32 +0000

    completed node fs writefile

---
+++ node-fs-writefile/cp.js
@@ -0,0 +1,8 @@
+const fs = require('fs');
+
+fs.readFile(process.argv[2], 'utf8', (err, data) => {
+  if (err) throw err;
+  fs.writeFile(process.argv[3], data, err => {
+    if (err) throw err;
+  });
+});
