5f36091e1f5657bf8aa5f46acb2f921539199d85
Tia <tiarekim@gmail.com>
2020-09-10 23:54:46 +0000

    completed getting a sigle note by id feature

--- express-notes-api/index.js
+++ express-notes-api/index.js
@@ -5,11 +5,28 @@ const app = express();
 fs.readFile('data.json', 'utf8', (err, data) => {
   if (err) throw err;
   const notesData = JSON.parse(data).notes;
+  const notes = [];
+
   app.get('/api/notes', (req, res, next) => {
-    const notes = [];
-    for (const prop in notesData) notes.push(data[prop]);
+    for (const prop in notesData) notes.push(notesData[prop]);
 
     res.status(200).json(notes);
+  });
+
+  app.get('/api/notes/:id', (req, res, next) => {
+    for (const prop in notesData) notes.push(notesData[prop]);
+    const id = Number(req.params.id);
+
+    if (id <= 0 || !Number.isInteger(id)) {
+      res.status(400).json({ error: 'id must be a positive integer' });
+    } else {
+      if (notes.findIndex(elm => elm.id === id) === -1) {
+        return res
+          .status(404)
+          .json({ error: `cannot find note with id ${id}` });
+      }
+      res.json(notes[notes.findIndex(elm => elm.id === id)]);
+    }
   });
 });
 
