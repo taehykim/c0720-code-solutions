82275ae383f4f3be00e34f2c0da08ecb11f07b3e
Tia <tiarekim@gmail.com>
2020-09-10 21:53:00 +0000

    redid the extra part of the express post json

--- express-post-json/public/main.js
+++ express-post-json/public/main.js
@@ -1,25 +1,41 @@
 /* global $ */
 const tbody = document.querySelector('tbody');
+const form = document.querySelector('form');
 
-$.ajax({
-  url: '/api/grades',
-  method: 'GET',
-  success: data => {
-    for (let i = 0; i < data.length; i++) {
+const handleSubmit = event => {
+  event.preventDefault();
+  const formData = new FormData(event.target);
+  const name = formData.get('name');
+  const course = formData.get('course');
+  const grade = formData.get('grade');
+  const data = { name: name, course: course, grade: grade };
+
+  $.ajax({
+    url: '/api/grades',
+    method: 'POST',
+    data: JSON.stringify(data),
+    dataType: 'json',
+    contentType: 'application/json',
+    success: function (data) {
+      /* eslint-disable no-console */
       const tr = document.createElement('tr');
       const nameTd = document.createElement('td');
       const courseTd = document.createElement('td');
       const gradeTd = document.createElement('td');
 
-      nameTd.textContent = data[i].name;
-      courseTd.textContent = data[i].course;
-      gradeTd.textContent = data[i].grade;
+      nameTd.textContent = data.name;
+      courseTd.textContent = data.course;
+      gradeTd.textContent = data.grade;
       tr.append(nameTd, courseTd, gradeTd);
       tbody.appendChild(tr);
+
+      form.reset();
+    },
+    error: function (err) {
+      /* eslint-disable no-console */
+      console.error(err);
     }
-  },
-  error: err => {
-    /* eslint-disable no-console */
-    console.error(err);
-  }
-});
+  });
+};
+
+form.addEventListener('submit', handleSubmit);
