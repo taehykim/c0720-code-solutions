0e2dca2fda021f38c808fcfee09721938c192483
Tia <tiarekim@gmail.com>
2020-09-11 00:21:09 +0000

    completed posting a new note feature

--- express-notes-api/index.js
+++ express-notes-api/index.js
@@ -4,17 +4,17 @@ const app = express();
 
 fs.readFile('data.json', 'utf8', (err, data) => {
   if (err) throw err;
-  const notesData = JSON.parse(data).notes;
+  const notesData = JSON.parse(data);
   const notes = [];
 
   app.get('/api/notes', (req, res, next) => {
-    for (const prop in notesData) notes.push(notesData[prop]);
+    for (const prop in notesData.notes) notes.push(notesData.notes[prop]);
 
     res.status(200).json(notes);
   });
 
   app.get('/api/notes/:id', (req, res, next) => {
-    for (const prop in notesData) notes.push(notesData[prop]);
+    for (const prop in notesData.notes) notes.push(notesData.notes[prop]);
     const id = Number(req.params.id);
 
     if (id <= 0 || !Number.isInteger(id)) {
@@ -27,6 +27,26 @@ fs.readFile('data.json', 'utf8', (err, data) => {
       }
       res.json(notes[notes.findIndex(elm => elm.id === id)]);
     }
+  });
+
+  app.use(express.json());
+  app.post('/api/notes', (req, res, next) => {
+    for (const prop in notesData.notes) notes.push(notesData.notes[prop]);
+
+    if (!req.body.content) { res.status(400).json({ error: 'content is a required field' }); }
+
+    const newNote = { id: notesData.nextId, content: req.body.content };
+    notes.push(newNote);
+
+    notesData.notes = notes;
+    notesData.nextId++;
+
+    fs.writeFile('data.json', JSON.stringify(notesData, null, 2), err => {
+      if (err) {
+        res.status(500).json({ error: 'An unexpected error occurred.' });
+      }
+      res.status(201).json(newNote);
+    });
   });
 });
 
