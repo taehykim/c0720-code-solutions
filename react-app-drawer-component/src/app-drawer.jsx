4c01f4654e110b6b8136641f4ea20fbdb709338d
Tia <tiarekim@gmail.com>
2020-09-18 07:26:46 +0000

    worked on the sliding menu feature

---
+++ react-app-drawer-component/src/app-drawer.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+
+class AppDrawer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { isOpen: false, opacity: 1 };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.setState(state => ({ isOpen: !state.isOpen }));
+  }
+
+  render() {
+    let menuBarClassName = 'menu-bar';
+    let menuContentsClassName = 'menu-contents';
+
+    if (this.state.isOpen) {
+      menuBarClassName += ' hide';
+    } else {
+      menuContentsClassName += ' hide';
+    }
+
+    return (
+      <div className="container">
+        <div className={menuBarClassName} onClick={this.handleClick}>
+          <i className="fas fa-bars fa-2x"></i>
+        </div>
+        <div className={menuContentsClassName}>
+          <ul className="menu-ul">
+            <li className="title">Menu</li>
+            <li onClick={this.handleMenuClick}>About</li>
+            <li onClick={this.handleClick}> Get Started</li>
+            <li onClick={this.handleClick}>Log In</li>
+            <li onClick={this.handleClick}>Sign Up</li>
+          </ul>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default AppDrawer;
